9f348d7420a925ce86b6122f0af2bd39de6ed984
Russell Emmer <remmer@revlocal.com>
2022-02-25 18:32:26 +0000

    First commit

--- computeTotalPrice.js
+++ computeTotalPrice.js
@@ -1,2 +1,31 @@
+const price = 299.00
+
+const discounts = {
+    0: 0,
+    1: 0,
+    2: 0.05,
+    3: 0.1,
+    4: 0.2,
+    5: 0.25
+}
+
+const kinds = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E'
+]
+
 export default function computeTotalPrice(productsArray) {
+    const countsByKind = kinds.map(kind => {
+        return productsArray.filter(product => product === kind).length
+    })
+    const greatestCount = Math.max(...countsByKind)
+    const eachDifferentCount = Array(greatestCount).fill(0).map((e, i) => i + 1)
+    return eachDifferentCount.map(count => {
+        const matchingCountsByKind = countsByKind.filter(c => c >= count).length
+        const absoluteDiscount = 1 - discounts[matchingCountsByKind]
+        return matchingCountsByKind * absoluteDiscount * price
+    }).reduce((total, price) => total + price, 0)
 }
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import computeTotalPrice from "./computeTotalPrice.js"
+
+const products = ['A','A','B','B','C','C','D','E']
+const result = computeTotalPrice(products)
+console.log(result)
